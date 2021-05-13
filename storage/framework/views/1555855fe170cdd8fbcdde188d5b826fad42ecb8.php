81c6cf61c7052de35898d16cfa16cf0b3412f4eb
Heidi Guevarra <72149454+HyGuevarra@users.noreply.github.com>
2021-05-13 21:40:50 +0800

    Add files via upload

---
+++ storage/framework/views/1555855fe170cdd8fbcdde188d5b826fad42ecb8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>TODO APP</title>
+        <!-- CSS And JavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+        <?php echo $__env->yieldContent('content'); ?>
+    </body>
+
+</html><?php /**PATH C:\TODO\resources\views/layouts/app.blade.php ENDPATH**/ ?>
